b5e720f0d17fb6030ba0adec68052f334bdf5e7c
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-01 12:43:48 +0100

    first commit

---
+++ src/pages/FicheLogements/FicheLogements.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FicheLogements = () => {
+    return (
+        <div className='ficheLogement'>
+            
+        </div>
+    );
+};
+
+export default FicheLogements;
