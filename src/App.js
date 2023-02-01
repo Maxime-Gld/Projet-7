2caa9f8d8ffbcc8f2ec89c02166a7eef88eb1ed6
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-01 20:20:48 +0100

    create file AppRoutes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './pages/Home/Home';
-import FicheLogements from './pages/FicheLogements/FicheLogements';
-import About from './pages/About/About';
-import Error from './pages/Error/Error';
+import AppRoutes from './AppRoutes';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <main>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/ficheLogement/:id' element={<FicheLogements/>}/>
-          <Route path='/about' element={<About/>}/>
-          
-          <Route path='*' element={<Error/>}/>
-        </Routes>
+        <AppRoutes/>
       </main>
       <Footer />
      
