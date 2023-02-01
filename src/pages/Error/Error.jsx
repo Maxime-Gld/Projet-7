45cf2b10d98665534e2e7cc2d22db68d9e02de16
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-01 17:19:52 +0100

    page react Error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div className='error'>
-            404 not found
+        <div className='error-container'>
+            <h1 className='error-container_title'>404</h1>
+            <p className='error-container_text'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d’accueil</Link>
         </div>
     );
 };
