86c22f15d32576a072f474bac026a00fe525a3d6
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-03 16:13:57 +0100

    add Card in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import { useState, useEffect } from 'react';
+import Card from '../../components/Card/Card';
+import data from '../../datas/Housing.json';
 
 const Home = () => {
+    const [ dataHousing, setDataHousing ] = useState([])
+
+    useEffect(() => {
+        setDataHousing(data)
+    }, [])
+
     return (
-        <div className='home'>
+        <main className='home'>
             <Banner />
-        </div>
+            <section className='cards-container'>
+                {dataHousing.map((housing) => (
+                    <Card
+                        key={housing.id}
+                        id={housing.id}
+                        picture={housing.cover}
+                        title={housing.title}
+                    />
+                ))}
+            </section>
+        </main>
     );
 };
 
