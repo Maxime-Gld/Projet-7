a190e7fe0defe36ccda538f01e0d7c830e6f5530
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-03 16:13:27 +0100

    create component Card

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types'
+
+const Card = ({ id, picture, title }) => {
+    return (
+        <article className='card'>
+            <Link to={`/housing/${id}`}>
+                <img src={picture} alt="rendu de l'appartement" />
+                <h2>{title}</h2>
+            </Link>
+        </article>
+    );
+};
+
+
+Card.propTypes = {
+    id: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+}
+
+export default Card;
