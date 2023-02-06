f294727d166049c55c47c96134b3a6ea7d41682c
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-06 11:07:52 +0100

    banner plus générique + proptype

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import banner_img from "../../assets/Images/banner_kasa.jpg"
+import { PropTypes } from 'prop-types';
 
-const Banner = () => {
+const Banner = ({picture, title}) => {
     return (
         <div className='banner-container'>
-            <img src={banner_img} alt="Banniere de la page d'accueil" />
-            <h1>Chez vous, partout et ailleurs</h1>
+            <img src={picture} alt="Banniere de la page d'accueil" />
+            <h1>{title}</h1>
         </div>
     );
 };
+
+Banner.prototype = {
+    picture: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+}
 
 export default Banner;
