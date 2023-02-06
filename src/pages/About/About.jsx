aab1156e9621d89c077bd07bc5db245ead0706bc
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-06 17:22:15 +0100

    import dropdown

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import banner_img from "../../assets/Images/banner_about.jpg";
 import Banner from '../../components/Banner/Banner';
+import DropDown from '../../components/DropDown/DropDown';
+import About_data from '../../datas/About';
+import icon from '../../assets/Images/vector.png';
 
 const About = () => {
+    const [ dataAbout, setDataAbout ] = useState([])
+
+    useEffect(() => {
+        setDataAbout(About_data)
+    }, [])
+
     return (
         <div className='about'>
             <Banner 
                 picture={banner_img}
                 title={""}
             />
+            <section className='about-container'>
+                {dataAbout.map((about) => (
+                    <DropDown
+                        title={about.title}
+                        icon={icon}
+                        text={about.text}
+                    />
+                ))}
+            </section>
         </div>
     );
 };
