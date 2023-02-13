ed7362561de9c85a62fab8164a27307470b04b2b
Maxime-Gld <Maxime.Gdev@hotmail.com>
2023-02-14 00:35:02 +0100

    page housing terminé

--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import Profil from '../../components/Profil/Profil';
 import Slideshow from '../../components/Slideshow/Slideshow';
+import Tag from '../../components/Tag/Tag';
+import star from '../../assets/Images/star-icon.png';
+import star_red from '../../assets/Images/star-red.png';
+import DropDown from '../../components/DropDown/DropDown';
 
 const Housing = () => {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [housing, setHousing] = useState({});
+  const [housing, setHousing] = useState([]);
+  const {
+    title,
+    location,
+    host,
+    tags,
+    rating,
+    pictures,
+    equipments,
+    description,
+  } = housing;
+
+  const range = [1, 2, 3, 4, 5];
+  const missingValue = range.length - rating;
+
+  const housingKeys = Object.keys(housing);
+  const titleEquipement = housingKeys[8];
+  const titleDescription = housingKeys[4];
 
   useEffect(() => {
     fetch('/housing.json')
@@ -22,21 +44,49 @@ const Housing = () => {
       .catch((err) => console.error(err));
   }, [id, navigate]);
 
-  const { title, location } = housing;
-
   return (
     <div className="housing">
-      <div>
-        <Slideshow datas={housing.pictures} />
-      </div>
+      <Slideshow datas={pictures} />
 
-      <section className="display_container">
-        <div className="display_text">
-          <h1>{title}</h1>
-          <p>{location}</p>
+      <section className="housing--information">
+        <div className="housing--information__container">
+          <div className="housing__text">
+            <h1>{title}</h1>
+            <p>{location}</p>
+          </div>
+          <Tag datas={tags} />
         </div>
-        <div></div>
+
+        <div className="housing--information__container">
+          <Profil datas={host} />
+          <div className="housing__rating">
+            {range.map((rangeElem) =>
+              rating >= rangeElem ? (
+                <img
+                  src={star_red}
+                  alt="etoile donnée sur 5"
+                  key={rangeElem.toString()}
+                />
+              ) : null
+            )}
+
+            {range.map((rangeElem) =>
+              missingValue >= rangeElem ? (
+                <img
+                  src={star}
+                  alt="étoile manquante sur 5"
+                  key={rangeElem.toString()}
+                />
+              ) : null
+            )}
+          </div>
+        </div>
       </section>
+
+      <div className="housing__dropdown">
+        <DropDown title={titleEquipement} datas={equipments} />
+        <DropDown title={titleDescription} datas={description} />
+      </div>
     </div>
   );
 };
